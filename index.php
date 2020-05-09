6cca41ad78aa0821100a1894939439ef2243b149
Scott thompson <Sthomspon42@mail.greenriver.edu>
2020-05-09 13:11:48 -0400

    pair programming tasks done

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 session_start();
+require_once('model/validation-functions.php');
 require_once('vendor/autoload.php');
 
 //Instantiate the F3 Base class
@@ -18,15 +19,13 @@ $f3->route('GET / ', function(){
 $f3->route('GET|POST /order ', function($f3){
 
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $colors = array("blue", "orange", "red");
-        var_dump($_POST);
-        if(empty($_POST['pet']) || !in_array($_POST['color'], $colors)){
+
+        if(validString($_POST['pet'])){
             echo "<p>Please supply a pet type</p>";
+            $f3->set("errors['pet']","Please enter an animal");
         } else{
             $_SESSION['pet'] = $_POST['pet'];
-            $_SESSION['color'] = $_POST['color'];
             $f3->reroute('order2');
-            session_destroy();
         }
     }
     $view = new Template();
@@ -39,8 +38,19 @@ $f3-> route('GET /summary', function (){
 });
 
 $f3->route('GET|POST /order2 ', function($f3){
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $colorSubmitted = $_POST['color'];
+        if(validColor($colorSubmitted)){
+           $_SESSION['color'] = $_POST['color'];
+            $f3->reroute('summary');
+            session_destroy();
+        }
+    }
+
     $view = new Template();
     echo $view->render('views/form2.html');
+
 });
 
 
